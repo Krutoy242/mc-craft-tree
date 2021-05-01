953e4ad0099f76984af86c36203b511ff53f3911
Krutoy242 <Krutoy242@gmail.com>
2021-05-01 15:54:18 +0300

    🚧 Dev Files update (TS working fine, JS - not)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     'env': {
         'browser': true,
+        node: true,
         'es6': true
     },
     'extends': [
@@ -35,7 +36,5 @@ module.exports = {
             'never'
         ],
         "no-unused-vars": "off",
-        "@typescript-eslint/no-unused-vars": "off",
-        "@typescript-eslint/no-unused-vars-experimental": "error"
     }
 }
